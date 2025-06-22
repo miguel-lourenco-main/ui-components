dedef01eef5bba0851a1c37ddbda89472c067f5f
Miguel Lourenco <mdsllc603@gmail.com>
2025-06-22 03:52:01 +0100

    fix: update Playwright base URL for local testing
    - Changed the base URL in Playwright configuration from port 3000 to 3001 for local testing.
    - This adjustment ensures compatibility with the updated local development environment.

--- playwright.config.ts
+++ playwright.config.ts
@@ -22,8 +22,8 @@ export default defineConfig({
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
     baseURL: process.env.CI_COMMIT_REF_SLUG 
-      ? `http://localhost:3000/${process.env.CI_COMMIT_REF_SLUG}`
-      : 'http://localhost:3000',
+      ? `http://localhost:3001/${process.env.CI_COMMIT_REF_SLUG}`
+      : 'http://localhost:3001',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
