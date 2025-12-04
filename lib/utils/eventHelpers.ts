54d82aebe4e9ca5588bd357e52e25dfc46fff3de
Miguel Lourenco <migasoulou@gmail.com>
2025-12-04 15:39:25 +0000

    dummy: dummy code for testing purposes

---
+++ lib/utils/eventHelpers.ts
@@ -0,0 +1,74 @@
+export function throttle<T extends (...args: any[]) => any>(
+  func: T,
+  limit: number
+): (...args: Parameters<T>) => void {
+  let inThrottle: boolean;
+  
+  return function executedFunction(...args: Parameters<T>) {
+    if (!inThrottle) {
+      func(...args);
+      inThrottle = true;
+      setTimeout(() => (inThrottle = false), limit);
+    }
+  };
+}
+
+export function debounce<T extends (...args: any[]) => any>(
+  func: T,
+  wait: number
+): (...args: Parameters<T>) => void {
+  let timeout: NodeJS.Timeout | null = null;
+  
+  return function executedFunction(...args: Parameters<T>) {
+    const later = () => {
+      timeout = null;
+      func(...args);
+    };
+    
+    if (timeout) {
+      clearTimeout(timeout);
+    }
+    timeout = setTimeout(later, wait);
+  };
+}
+
+export function once<T extends (...args: any[]) => any>(func: T): T {
+  let called = false;
+  let result: ReturnType<T>;
+  
+  return ((...args: Parameters<T>) => {
+    if (!called) {
+      result = func(...args);
+      called = true;
+    }
+    return result;
+  }) as T;
+}
+
+export function memoize<T extends (...args: any[]) => any>(
+  func: T,
+  keyGenerator?: (...args: Parameters<T>) => string
+): T {
+  const cache = new Map<string, ReturnType<T>>();
+  
+  return ((...args: Parameters<T>) => {
+    const key = keyGenerator ? keyGenerator(...args) : JSON.stringify(args);
+    
+    if (cache.has(key)) {
+      return cache.get(key)!;
+    }
+    
+    const result = func(...args);
+    cache.set(key, result);
+    return result;
+  }) as T;
+}
+
+export function pipe<T>(...fns: Array<(arg: T) => T>): (arg: T) => T {
+  return (arg: T) => fns.reduce((acc, fn) => fn(acc), arg);
+}
+
+export function compose<T>(...fns: Array<(arg: T) => T>): (arg: T) => T {
+  return (arg: T) => fns.reduceRight((acc, fn) => fn(acc), arg);
+}
+
